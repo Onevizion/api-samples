2ab8de1f4aa94d6b576a4140f3d05187ba950a41
Mike Greene <bombadil@oldforest.net>
2017-12-14 11:20:48 -0500

    Better Version Ordering

--- v3/patch-notes.py
+++ v3/patch-notes.py
@@ -1,5 +1,6 @@
 import json
 import datetime
+import operator
 import onevizion
 
 # Handle command arguments
@@ -11,13 +12,6 @@ EpiLog = onevizion.PasswordExample + """\n\n
 parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,description=Description,epilog=EpiLog)
 parser.add_argument("Version", help="Version to Release")
 parser.add_argument("PreviousVersion", help="Previous Version released last")
-parser.add_argument(
-	"-f", 
-	"--filter", 
-	metavar="filter", 
-	help="Filter to add to Version ID.", 
-	default=""
-	)
 parser.add_argument(
 	"-i", 
 	"--includeprev", 
@@ -41,7 +35,6 @@ Product = args.product
 ThisVersion = args.Version
 Versions = [args.Version]
 PreviousVersion = args.PreviousVersion
-VersionFilter= args.filter
 
 if len(args.to) > 0:
 	PassedTo=args.to.split(",")
@@ -51,6 +44,7 @@ else:
 PasswordData = onevizion.GetParameters(PasswordsFile)
 onevizion.Config["Verbosity"]=args.verbose
 onevizion.Config["SMTPToken"]="SMTP"
+Message=onevizion.Message
 # Make sure Passwordsfile has correct sections
 PDError = (
 	onevizion.CheckPasswords(PasswordData,'SMTP',['Server','Port','UserName','Password','To']),
@@ -68,7 +62,7 @@ Trace = onevizion.Config["Trace"]
 Issues = OrderedDict()
 
 
-def Notif (Title,Message,To):
+def Notif (Title,Body,To):
 	"""Sends Mail notifying if an error occurs"""
 
 	msg = onevizion.EMail()
@@ -78,7 +72,7 @@ def Notif (Title,Message,To):
 	else:
 		msg.to = [To]
 	msg.subject = Title
-	msg.message = Message
+	msg.message = Body
 	
 	msg.info.update(Trace)
 
@@ -118,116 +112,148 @@ def VersionInfo(GivenVersion):
 
 	return ThisVersion
 
+def VersionSplit(ThisVersion):
+	VersionBreak=ThisVersion.split(".")
+	VersionParts=[]
+	VersionParts.append(int(VersionBreak[0]))
+	VersionParts.append(int(VersionBreak[1]))
+	RelVer = VersionBreak[2].split('-RC')
+	VersionParts.append(int(RelVer[0]))
+	if len(RelVer) == 2:
+		VersionParts.append(int(RelVer[1]))
+	return VersionParts
+
+def GetVersionsList(VersionsStr,VersionDate):
+	VersionRequest = onevizion.Trackor(
+		trackorType = 'Version', 
+		paramToken = 'trackor.onevizion.com'
+		)
+	VersionRequest.read(
+		search="equal(TRACKOR_KEY, 8.*) and equal(Product.TRACKOR_KEY, OneVizion) and is_not_null(VER_REL_DATE)",
+		fields=[
+			'TRACKOR_KEY',
+			'VER_REL_DATE'
+			],
+		sort={'TRACKOR_KEY':'asc'}
+		)
+
+	AllVers=[]
+	for Version in VersionRequest.jsonData:
+		AllVers.append(VersionSplit(Version['TRACKOR_KEY']))
+		VersionDate[Version['TRACKOR_KEY']]=Version['VER_REL_DATE']
+
+	AllVers.sort(key=operator.itemgetter(0,1,2))
+	Message(json.dumps(AllVers,indent=2),2)
+
+	for Version in AllVers:
+		if "%d.%d.%d"%(Version[0],Version[1],Version[2]) not in VersionsStr:
+			VersionsStr.append("%d.%d.%d"%(Version[0],Version[1],Version[2]))
+
+	Message(json.dumps(VersionsStr,indent=2), 2)
+
+
+
 NewerVersion=VersionInfo(ThisVersion)
 OlderVersion=VersionInfo(PreviousVersion)
 
 VersionList=OrderedDict()
 
-VersionRequest = onevizion.Trackor(
-	trackorType = 'Version', 
+
+AllVersions=[]
+VersionDates={}
+GetVersionsList(AllVersions,VersionDates)
+
+#Message(json.dumps(AllVersions,indent=2))
+#Message(json.dumps(VersionDates,indent=2))
+
+
+
+# Break out an ordered list of only the needed version updates.
+NeededVersions = []
+if args.includeprev > 0:
+	PrevBuffer = 0
+else:
+	PrevBuffer = 1
+for i in range(AllVersions.index(OlderVersion['Version'])+PrevBuffer,AllVersions.index(NewerVersion['Version'])+1):
+	NeededVersions.append(AllVersions[i])
+Message("{NumVers} versions to list.".format(NumVers=len(NeededVersions)))
+Message(json.dumps(NeededVersions,indent=2), 1)
+
+
+
+NeededVersions.reverse()
+for Ver in NeededVersions:
+	VersionList[Ver] = datetime.datetime.strptime(VersionDates[Ver],'%Y-%m-%d')
+
+Versions=[]
+
+# Get Issues for any Versions attached to this Email
+IssueList = onevizion.Trackor(
+	trackorType = 'Issue', 
 	URL = 'trackor.onevizion.com', 
 	userName=PasswordData["trackor.onevizion.com"]["UserName"], 
 	password=PasswordData["trackor.onevizion.com"]["Password"]
 	)
-
-Search = 'equal(Product.TRACKOR_KEY, {Product}) and less_or_equal(VER_REL_DATE,{NewerVersionDate})'.format(
-		NewerVersionDate=NewerVersion['ReleaseDate'].strftime('%Y-%m-%d'),
-		Product=Product
+for Version,RelDate in VersionList.items():
+	Ver={}
+	IssueList.read(
+		filters={
+			"Version.TRACKOR_KEY":Version,
+			"VQS_IT_DONT_INCLUDE_IN_REL_NOTES":"0"
+			} ,
+		fields=[
+			'TRACKOR_KEY',
+			'VQS_IT_XITOR_NAME',
+			'VQS_IT_RELEASE_NOTES'
+			]
 		)
-if args.includeprev > 0:
-	Search += ' and greater_or_equal(VER_REL_DATE, {OlderVersionDate})'.format(
-		OlderVersionDate=OlderVersion['ReleaseDate'].strftime('%Y-%m-%d')
-		)
-else:
-	Search += ' and greater(VER_REL_DATE, {OlderVersionDate})'.format(
-		OlderVersionDate=OlderVersion['ReleaseDate'].strftime('%Y-%m-%d')
-		)
-if len(VersionFilter) > 0:
-	Search += ' and equal(TRACKOR_KEY, {VersionFilter})'.format(
-		VersionFilter=VersionFilter
-		)
-VersionRequest.read(
-	search=Search,
-	fields=[
-		'TRACKOR_KEY',
-		'VER_REL_DATE'
-		],
-	sort={'VER_REL_DATE':'desc','TRACKOR_KEY':'desc'}
-	)
-if len(VersionRequest.errors) > 0:
-	Notif('Patch Notes Failed','Patch Notes Failed.','development@onevizion.com')
-else:
-	for Ver in VersionRequest.jsonData:
-		VersionList[Ver['TRACKOR_KEY']] = datetime.datetime.strptime(Ver['VER_REL_DATE'],'%Y-%m-%d')
-
-	Versions=[]
-
-	# Get Issues for any Versions attached to this Email
-	IssueList = onevizion.Trackor(
-		trackorType = 'Issue', 
-		URL = 'trackor.onevizion.com', 
-		userName=PasswordData["trackor.onevizion.com"]["UserName"], 
-		password=PasswordData["trackor.onevizion.com"]["Password"]
-		)
-	for Version,RelDate in VersionList.items():
-		Ver={}
-		IssueList.read(
-			filters={
-				"Version.TRACKOR_KEY":Version,
-				"VQS_IT_DONT_INCLUDE_IN_REL_NOTES":"0"
-				} ,
-			fields=[
-				'TRACKOR_KEY',
-				'VQS_IT_XITOR_NAME',
-				'VQS_IT_RELEASE_NOTES'
-				]
-			)
-		if len(IssueList.errors) > 0:
-			Notif('Patch Notes Failed','Patch Notes Failed.','development@onevizion.com')
-		else:
-			Ver['Version']=Version
-			Ver['ReleaseDate']=RelDate
-			Issues=[]
-			for Issue in IssueList.jsonData:
-				Iss={}
-				Iss['IssueID']=Issue['TRACKOR_KEY']
-				Iss['Summary']=Issue['VQS_IT_XITOR_NAME']
-				Iss['Notes']=Issue['VQS_IT_RELEASE_NOTES']
-				Issues.append(Iss)
-			Ver['Issues']=Issues
-		if Ver != {}:
-			Versions.append(Ver)
-
-
-	# Send The Email since everthing is successful			
-	Title = "OneVizion "+ThisVersion+" deployed in "+NewerVersion['Type']
-	Message = "OneVizion "+ThisVersion+" deployed in "+NewerVersion['Type']
-	if NewerVersion['Type'] == 'UAT':
-		Message = Message+", we are planing production release on "+NewerVersion['ReleaseDate'].strftime('%Y-%m-%d')+"."
+	if len(IssueList.errors) > 0:
+		Notif('Patch Notes Failed','Patch Notes Failed.','development@onevizion.com')
 	else:
-		Message = Message+"."
-	Message = Message+" Following is list of changes since "+OlderVersion['Version']+" version:"
-
-	for Version in Versions:
-		Message += "\n\n{Version}:  {ReleaseDate}\n================================================".format(
-			Version=Version['Version'],
-			ReleaseDate=Version['ReleaseDate'].strftime('%Y-%m-%d')
-			)
-		for Issue in Version['Issues']:
-			Message += "\n{IssueID}:  {Summary}\n".format(
-				IssueID=Issue['IssueID'],
-				Summary=Issue['Summary']
-				)
-			if Issue['Notes'] is not None:
-				Message += "{Notes}\n".format(Notes=Issue['Notes'])
-			#else:
-				Message += '\n'
+		Ver['Version']=Version
+		Ver['ReleaseDate']=RelDate
+		Issues=[]
+		for Issue in IssueList.jsonData:
+			Iss={}
+			Iss['IssueID']=Issue['TRACKOR_KEY']
+			Iss['Summary']=Issue['VQS_IT_XITOR_NAME']
+			Iss['Notes']=Issue['VQS_IT_RELEASE_NOTES']
+			Issues.append(Iss)
+		Ver['Issues']=Issues
+	if Ver != {}:
+		Versions.append(Ver)
 
 
-	if len(PassedTo) == 0:
-		PassedTo = PasswordData["SMTP"]["To"]
-	Notif(
-		Title=Title,
-		Message=Message,
-		To=PassedTo
+# Send The Email since everthing is successful			
+Title = "OneVizion "+ThisVersion+" deployed in "+NewerVersion['Type']
+Body = "OneVizion "+ThisVersion+" deployed in "+NewerVersion['Type']
+if NewerVersion['Type'] == 'UAT':
+	Body = Body+", we are planing production release on "+NewerVersion['ReleaseDate'].strftime('%Y-%m-%d')+"."
+else:
+	Body = Body+"."
+Body = Body+" Following is list of changes since "+OlderVersion['Version']+" version:"
+
+for Version in Versions:
+	Body += "\n\n{Version}:  {ReleaseDate}\n================================================".format(
+		Version=Version['Version'],
+		ReleaseDate=Version['ReleaseDate'].strftime('%Y-%m-%d')
 		)
+	for Issue in Version['Issues']:
+		Body += "\n{IssueID}:  {Summary}\n".format(
+			IssueID=Issue['IssueID'],
+			Summary=Issue['Summary']
+			)
+		if Issue['Notes'] is not None:
+			Body += "{Notes}\n".format(Notes=Issue['Notes'])
+		#else:
+			Body += '\n'
+
+
+if len(PassedTo) == 0:
+	PassedTo = PasswordData["SMTP"]["To"]
+Notif(
+	Title=Title,
+	Body=Body,
+	To=PassedTo
+	)
+
